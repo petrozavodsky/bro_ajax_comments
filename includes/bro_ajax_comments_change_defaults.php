84537971ea66eca36c6b8d6483447339d745691f
petrozavodsky <petrozavodsky@gmail.com>
2018-12-25 17:35:37 +0300

    статические сойства

--- includes/bro_ajax_comments_change_defaults.php
+++ includes/bro_ajax_comments_change_defaults.php
@@ -1,98 +1,91 @@
 <?php
 
-if (!defined('ABSPATH')) {
-    exit;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
 }
 
-class bro_ajax_comments_change_defaults
-{
+class bro_ajax_comments_change_defaults {
 
-    public function __construct()
-    {
-        require_once("bro_ajax_walker_comment.php");
-        add_filter('wp_list_comments_args', array($this, 'wp_list_comments_args_filter'));
-        add_filter('comment_form_defaults', array($this, 'comment_form_defaults_filter'));
-        add_filter('cancel_comment_reply_link', "__return_empty_string", 10, 3);
-        add_filter('comment_form_default_fields', array($this, "comment_form_default_fields_filter"));
+	public function __construct() {
+		require_once( "bro_ajax_walker_comment.php" );
+		add_filter( 'wp_list_comments_args', array( __CLASS__, 'wp_list_comments_args_filter' ) );
+		add_filter( 'comment_form_defaults', array( __CLASS__, 'comment_form_defaults_filter' ) );
+		add_filter( 'cancel_comment_reply_link', "__return_empty_string", 10, 3 );
+		add_filter( 'comment_form_default_fields', array( $this, "comment_form_default_fields_filter" ) );
 
-        add_action('comment_form_before_fields', array($this, "comment_form_before_fields_filter"));
-        add_action('comment_form_after_fields', array($this, "comment_form_after_fields_filter"));
-	    add_action('preprocess_comment', array($this,'allowed_tags'));
+		add_action( 'comment_form_before_fields', array( __CLASS__, "comment_form_before_fields_filter" ) );
+		add_action( 'comment_form_after_fields', array( __CLASS__, "comment_form_after_fields_filter" ) );
+		add_action( 'preprocess_comment', array( __CLASS__, 'allowed_tags' ) );
 
-    }
+	}
 
-
-
-	public function allowed_tags($dannie) {
+	public static function allowed_tags( $dannie ) {
 		global $allowedtags;
 
 		// Разрешаем новые HTML теги:
-		$allowedtags['pre'] = array('class' => array());
-		$allowedtags['ul'] = array('class' => array());
-		$allowedtags['ol'] = array('class' => array());
-		$allowedtags['li'] = array('class' => array());
-		$allowedtags['p'] = array('class' => array());
-		$allowedtags['li'] = array('id' => array());
-		$allowedtags['b'] = array('id' => array());
-		$allowedtags['strong'] = array('id' => array());
-		$allowedtags['strike'] = array('id' => array());
-		$allowedtags['em'] = array('id' => array());
+		$allowedtags['pre']    = array( 'class' => array() );
+		$allowedtags['ul']     = array( 'class' => array() );
+		$allowedtags['ol']     = array( 'class' => array() );
+		$allowedtags['li']     = array( 'class' => array() );
+		$allowedtags['p']      = array( 'class' => array() );
+		$allowedtags['li']     = array( 'id' => array() );
+		$allowedtags['b']      = array( 'id' => array() );
+		$allowedtags['strong'] = array( 'id' => array() );
+		$allowedtags['strike'] = array( 'id' => array() );
+		$allowedtags['em']     = array( 'id' => array() );
 
 		return $dannie;
 	}
 
-	public function comment_form_before_fields_filter()
-    {
-        echo "<div class='comments__fields'>";
+	public static function comment_form_before_fields_filter() {
+		echo "<div class='comments__fields'>";
 
-    }
+	}
 
-    public function comment_form_after_fields_filter()
-    {
-        echo "</div>";
-    }
+	public static function comment_form_after_fields_filter() {
+		echo "</div>";
+	}
 
-    public function comment_form_default_fields_filter($fields)
-    {
-        foreach ($fields as $key => $val) {
-            $val = str_replace("<p", '<div', $val);
-            $val = str_replace("</p", '</div', $val);
-            $fields[$key] = $val;
-        }
-        return $fields;
-    }
+	public function comment_form_default_fields_filter( $fields ) {
+		foreach ( $fields as $key => $val ) {
+			$val            = str_replace( "<p", '<div', $val );
+			$val            = str_replace( "</p", '</div', $val );
+			$fields[ $key ] = $val;
+		}
 
-    public function comment_form_defaults_filter($args)
-    {
+		return $fields;
+	}
+
+	public function comment_form_defaults_filter( $args ) {
 //        $args['fields']['url'] = '';
-        $args['comment_field'] = '<div class="comment-form__text_area"><textarea id="comment" name="comment" maxlength="65525" aria-required="true" ></textarea> </div>';
-        $args['logged_in_as'] = '';
-        $args['format'] = 'html5';
-        $args['submit_button'] = '<button name="%1$s" type="submit" id="%2$s" class="%3$s"  > %4$s </button>';
-        $args['title_reply_before'] = '';
-        $args['title_reply_after'] = '';
+		$args['comment_field']      = '<div class="comment-form__text_area"><textarea id="comment" name="comment" maxlength="65525" aria-required="true" ></textarea> </div>';
+		$args['logged_in_as']       = '';
+		$args['format']             = 'html5';
+		$args['submit_button']      = '<button name="%1$s" type="submit" id="%2$s" class="%3$s"  > %4$s </button>';
+		$args['title_reply_before'] = '';
+		$args['title_reply_after']  = '';
 
-        $args['cancel_reply_before'] = "";
-        $args['cancel_reply_after'] = "";
-        $args['cancel_reply_link'] = "";
-        $args['title_reply_to'] = "";
-        $args['title_reply'] = "";
-        $args['label_submit'] = "Отправить";
+		$args['cancel_reply_before'] = "";
+		$args['cancel_reply_after']  = "";
+		$args['cancel_reply_link']   = "";
+		$args['title_reply_to']      = "";
+		$args['title_reply']         = "";
+		$args['label_submit']        = "Отправить";
 
-        return $args;
-    }
+		return $args;
+	}
 
-    public function wp_list_comments_args_filter($args)
-    {
+	public function wp_list_comments_args_filter( $args ) {
 
-        $args['walker'] = new bro_ajax_walker_comment;
-        $args['format'] = "html5";
-        $args['type'] = "comment";
-        $args['avatar_size'] = 80;
-        $args['reply_text'] = 'Ответить';
-        $args['style'] = "div";
-        return $args;
-    }
+		$args['walker']      = new bro_ajax_walker_comment;
+		$args['format']      = "html5";
+		$args['type']        = "comment";
+		$args['avatar_size'] = 80;
+		$args['reply_text']  = 'Ответить';
+		$args['style']       = "div";
+
+		return $args;
+	}
 
 
 }
